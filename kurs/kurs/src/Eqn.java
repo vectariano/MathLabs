7e90b75afed0c04049765bf2b3030cc1f549f1ab
vectariano <victorevstigneev36@gmail.com>
2025-03-23 17:34:55 +0300

    Fixed RKF problem

--- kurs/kurs/src/Eqn.java
+++ kurs/kurs/src/Eqn.java
@@ -11,8 +11,8 @@ public class Eqn implements FuncInterf {
     @Override
     public void f(int n, double t, double[] x, double[] dx) {
         dx[0] = x[1];
-        dx[1] = K / M * x[0] + g * (1 - Math.cos(x[3])) - (L + x[0]) * Math.pow(x[2], 2);
-        dx[2] = x[2];
-        dx[3] = g / (L + x[0]) * Math.sin(x[3]) + 2 / (L + x[0]) * x[1] * x[2];
+        dx[1] = -K / M * x[0] - g * (1 - Math.cos(x[2])) + (L + x[0]) * Math.pow(x[3], 2);
+        dx[2] = x[3];
+        dx[3] = -g / (L + x[0]) * Math.sin(x[2]) - 2 / (L + x[0]) * x[1] * x[3];
     }
 }
--- kurs/kurs/src/Main.java
+++ kurs/kurs/src/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) throws RKF45Exception {
 
 //        Decomp/Solve
-
         double A, B, C, D, L, M, K, g;
         int n = 4, ndim = 10, flag = 0;
         int[] pivot = new int[n];
@@ -40,8 +39,8 @@ public class Main {
 
 //        Quanc8
         Function<Double, Double> func = x -> 1 / Math.pow((x - 1) * Math.pow((x + 1), 2), 1.0 / 3.0);
-        double ABSERR = 0.00001;
-        double RELERR = 0.00001;
+        double ABSERR = 0;
+        double RELERR = 10e-10;
         double a_ = 2.0;
         double b_ = 4.0;
 
@@ -54,8 +53,8 @@ public class Main {
 
 
 //        Zeroin
-        Function<Double, Double> func2 = x -> Math.log(3 * x - 1) + Math.exp(2 * x - 1);
-        double left = 0.3, right = 0.4, t = 1.0e-10;
+        Function<Double, Double> func2 = x -> Math.log10(3 * x - 1) + Math.exp(2 * x - 1);
+        double left = 0.37, right = 0.4, t = 10e-7;
 
         Zeroin solution = new Zeroin(func2, left, right, t);
 
@@ -68,35 +67,37 @@ public class Main {
         double[] y = new double[4];
         y[0] = A;
         y[1] = B;
-        y[2] = C;
-        y[3] = D;
+        y[2] = D;
+        y[3] = C;
         K = 39.24;
         double[] YP = new double[NEQN];
         double T = 0.0;
         double TOUT = 4.0;
-        double RELERR_ = 0.0001;
-        double ABSERR_ = 0.0001;
-        double H = 0.01;
+        double RELERR_ = 10e-10;
+        double ABSERR_ = 0;
+        double H = 0.1;
         int NFE = 0;
         int MAXNFE = 5000;
         int IFLAG = 1;
         g = 9.81;
         FuncInterf system = new Eqn(K, M, L, g);
 
+        System.out.println("K = " + K);
         System.out.println("RKF45");
-        System.out.println(String.format("\t %5s \t %7s \t %8s \t %6s", "Step", "y1", "y2", "flag"));
+        System.out.println(String.format("\t %5s \t %7s \t %8s \t %6s", "Step", "x", "O", "flag"));
 
-        for (int i = 0; i < 41; i++) {
+        for (int i = 1; i < 41; i++) {
             RKF45 rkf45 = new RKF45(
                     system, NEQN, y, YP, T, TOUT, RELERR_, ABSERR_, H, NFE, MAXNFE, IFLAG
             );
             rkf45.complete();
 
-            System.out.println(String.format(
-                    "%10.2f %s %10.6f %s %10.6f %s\t %d",
-                    T, "|", y[1], "|", y[3], "|", IFLAG)
-            );
             T += H;
+
+            System.out.println(String.format(
+                    "%10.1f %s %10.6f %s %10.6f %s\t %d",
+                    T, "|", y[0], "|", y[2], "|", IFLAG)
+            );
         }
     }
 }
